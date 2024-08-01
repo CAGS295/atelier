f70e60757a1f75c891bb1f4390946e5b26d6bd8a
IFFranciscoME <if.francisco.me@gmail.com>
2024-08-01 15:02:48 -0600

    Model core definition

--- python/atelier/models/autoencoder.py
+++ python/atelier/models/autoencoder.py
@@ -1,84 +1,96 @@
-import torch
+
+# --- ------------------------------------------------------------------- --- #
+# --- autoencoder.py ---------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
+
 import torch.nn as nn
-import torch.optim as optim
-from torch.nn import functional as F
-import numpy as np
 
-seed = 42
-np.random.seed(seed)
-torch.manual_seed(seed)
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-dtype = torch.float16
-
-n_past = 30
-n_dims = 51
-n_future = 10
-
-# Hiper-parameters
-encoder_units_1 = 200
-decoder_units_1 = 200
-dropout_value = 0.4
-optimizer = "adam"
-batch_size = 64
-n_epochs = 150
-learning_rate = 0.0001
-cost_function = "MAE"
-
-# Extras
-batch_normalization = True
-early_stopping = "MAE should improve every 30 epochs"
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
 
 class Encoder(nn.Module):
     
-    def __init__(self, n_past, n_dims, encoder_units_1):
+    def __init__(self, input_size, hidden_size, num_layers=1):
         
         super(Encoder, self).__init__()
 
-        self.encoder_inputs = nn.Linear(n_past * n_dims, encoder_units_1)
-        self.encoder_l1 = nn.LSTM(input_size=n_dims, 
-                                  hidden_size=encoder_units_1, 
-                                  num_layers=1, 
-                                  batch_first=True)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
 
-    def forward(self, inputs):
+        self.encoder_l1 = nn.LSTM(input_size=input_size, 
+                                  hidden_size=hidden_size,
+                                  num_layers=num_layers, 
+                                  bias=True,
+                                  batch_first=True,
+                                  dropout=0,
+                                  bidirectional=False,
+                                  proj_size=0,)
+
+    def forward(self,
+                inputs):
+        """
+
+        """
         
-        outputs1 = self.encoder_inputs(inputs.view(-1, n_past * n_dims))
-        outputs1, (state_h1, state_c1) = self.encoder_l1(outputs1.unsqueeze(0))
-        states1 = [state_h1, state_c1]
-        
-        return outputs1, states1
+        flat_input = inputs.view(inputs.shape[0],
+                                 inputs.shape[1],
+                                 self.input_size)
+
+        outputs, states = self.encoder_l1(flat_input)
+
+        return outputs, states
+
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
 
 class Decoder(nn.Module):
     
-    def __init__(self, encoder_units_1, decoder_units_1): 
+    def __init__(self, input_size, hidden_size, output_size=1, num_layers=1): 
         super(Decoder, self).__init__()
 
-        self.decoder_state_h = nn.Linear(encoder_units_1, decoder_units_1)
-        self.decoder_state_c = nn.Linear(encoder_units_1, decoder_units_1)
-        self.decoder_l1 = nn.LSTM(input_size=n_dims,
-                                  hidden_size=decoder_units_1,
-                                  num_layers=1, 
-                                  batch_first=True)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        
+        self.decoder_l1 = nn.LSTM(input_size=input_size,
+                                  hidden_size=hidden_size,
+                                  num_layers=num_layers,
+                                  bias=True,
+                                  batch_first=True,
+                                  dropout=0,
+                                  bidirectional=False,
+                                  proj_size=0,)
 
-    def forward(self, inputs, initial_state):
+        self.linear = nn.Linear(hidden_size, output_size)
 
-        outputs1 = self.decoder_l1(inputs, initial_state=initial_state)
+    def forward(self, inputs, states):
+        
+        outputs, states = self.decoder_l1(inputs.unsqueeze(0), states)
+        flat_outputs = self.linear(outputs.squeeze(0))
+    
+        return flat_outputs, states
 
-        return outputs1
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
 
-class OutputLayer(nn.Module):
+class Autoencoder(nn.Module):
 
-    def __init__(self, output_dim):
-        super(OutputLayer, self).__init__()
+    def __init__(self, input_size=1, hidden_size=1, output_size=1):
+        super(Autoencoder, self).__init__()
 
-        self.output_layer = nn.Linear(output_dim, 10)
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        
+        self.encoder = Encoder(input_size=self.input_size, 
+                               hidden_size=self.hidden_size)
+        
+        self.decoder = Decoder(input_size=self.input_size, 
+                               hidden_size=self.hidden_size,
+                               output_size=self.output_size)
 
-    def forward(self, inputs):
-
-        outputs1 = self.output_layer(inputs)
-
-        return outputs1
-
-encoder_inputs = torch.randn((n_past, n_dims), device=device, dtype=dtype)
-encoder_l1 = Encoder(n_past, n_dims, encoder_units_1).to(device)
