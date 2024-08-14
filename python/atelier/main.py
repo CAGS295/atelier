d5598ed8c9e39bb4e4965a897847600480a8cff4
IFFranciscoME <if.francisco.me@gmail.com>
2024-08-13 21:52:31 -0600

    Misc changes

--- python/atelier/main.py
+++ python/atelier/main.py
@@ -1,6 +1,11 @@
 
 import atelier as atelier
 
-py_ob = atelier.atelier.fill_orderbook_py()
-print(py_ob)
+r_ob = atelier.atelier.Orderbook()
 
+print(r_ob.orderbook_id)
+print(r_ob.orderbook_ts)
+print(r_ob.levels)
+
+ob = r_ob.new_random()
+print(ob.levels)
--- tests/base.py
+++ tests/base.py
@@ -1,3 +1,123 @@
 import atelier 
 sum_as_string = atelier.sum_as_string(1,2)
 print(sum_as_string)
+
+
+"""
+
+
+    }
+
+    pub fn random_case(&mut self, entries: u32) {
+        
+        for _ in 0..entries {
+    
+            let (ts, price, amount, is_buy) = randomize_order();
+            let order = Order {order_id: 123, order_ts, side, price, amount,};
+
+            if is_buy {
+                self.bids.levels.push(
+                    Level { 
+                        id: 123,
+                        price,
+                        orders: VecDeque::from_iter(vec![order]).into(),
+                    }
+                );
+            } else {
+                self.asks.levels.push(
+                    Level {
+                        id: 123,
+                        price,
+                        orders: VecDeque::from_iter(vec![order]).into(),
+                    }
+                );
+            }
+        }
+
+fn fill_orderbook_py() -> PyResult<Orderbook> {
+    let mut ob = Orderbook {
+        id: 1234,
+        ts: 1234.1234,
+        symbol: String::from("BTCUSDT"),
+        bids: Bids { levels: Vec::new() },
+        asks: Asks { levels: Vec::new() },
+    };
+
+    for _ in 0..10 {
+        
+        let (ts, price, amount, is_buy) = randomize_order();
+        
+        let order = Order {
+            id: 123,
+            ts,
+            price,
+            amount,
+            is_buy,};
+
+        if is_buy {
+            ob.bids.levels.push(
+                Level { 
+                    id: 123,
+                    price,
+                    orders: VecDeque::from_iter(vec![order]).into(),
+                }
+            );
+        } else {
+            ob.asks.levels.push(
+                Level {
+                    id: 123,
+                    price,
+                    orders: VecDeque::from_iter(vec![order]).into(),
+                }
+            );
+        }
+    }
+
+    Ok(ob)
+}
+
+#[pyfunction]
+fn fill_orderbook_py() -> PyResult<Orderbook> {
+    let mut ob = Orderbook {
+        id: 1234,
+        ts: 1234.1234,
+        symbol: String::from("BTCUSDT"),
+        bids: Bids { levels: Vec::new() },
+        asks: Asks { levels: Vec::new() },
+    };
+
+    for _ in 0..10 {
+        
+        let (ts, price, amount, is_buy) = randomize_order();
+        
+        let order = Order {
+            id: 123,
+            ts,
+            price,
+            amount,
+            is_buy,};
+
+        if is_buy {
+            ob.bids.levels.push(
+                Level { 
+                    id: 123,
+                    price,
+                    orders: VecDeque::from_iter(vec![order]).into(),
+                }
+            );
+        } else {
+            ob.asks.levels.push(
+                Level {
+                    id: 123,
+                    price,
+                    orders: VecDeque::from_iter(vec![order]).into(),
+                }
+            );
+        }
+    }
+
+    Ok(ob)
+}
+
+
+"""
